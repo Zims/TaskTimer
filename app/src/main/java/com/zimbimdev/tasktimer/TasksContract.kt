eec957a546f2bd1030dcae345de7d06ebe1077b1
Zims <real.zims@gmail.com>
2019-06-25 13:41:03 +0300

    Initial commit

---
+++ app/src/main/java/com/zimbimdev/tasktimer/TasksContract.kt
@@ -0,0 +1,17 @@
+package com.zimbimdev.tasktimer
+
+import android.provider.BaseColumns
+
+object TasksContract {
+
+    internal const val TABLE_NAME = "Tasks"
+
+    //task fields
+    object Columns {
+        const val ID = BaseColumns._ID
+        const val TASK_NAME = "Name"
+        const val TASK_DESCRIPTION = "Description"
+        const val TASK_SORT_ORDER = "SortOrder"
+    }
+
+}
