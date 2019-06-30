4d5cde060e4804c44c50ff9bcc0f0c62fab95ef8
Zims <real.zims@gmail.com>
2019-06-30 11:10:53 +0300

    add update tasks

--- app/src/main/java/com/zimbimdev/tasktimer/AddEditFragment.kt
+++ app/src/main/java/com/zimbimdev/tasktimer/AddEditFragment.kt
@@ -1,9 +1,12 @@
 package com.zimbimdev.tasktimer
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +14,7 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_add_edit.*
 
 private const val TAG = "AddEditFragment"
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_TASK = "task"
 
@@ -21,38 +25,103 @@ private const val ARG_TASK = "task"
  * to handle interaction events.
  * Use the [AddEditFragment.newInstance] factory method to
  * create an instance of this fragment.
+ *
  */
 class AddEditFragment : Fragment() {
     private var task: Task? = null
     private var listener: OnSaveClicked? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onCreate starts")
+        Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         task = arguments?.getParcelable(ARG_TASK)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        Log.d(TAG, "onCreateView starts")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        Log.d(TAG, "onCreateView: starts")
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_edit, container, false)
     }
 
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewCreated: called")
+        if (savedInstanceState == null) {
+            val task = task
+            if (task != null) {
+                Log.d(TAG, "onViewCreated: Task details found, editing task ${task.id}")
+                addedit_name.setText(task.name)
+                addedit_description.setText(task.description)
+                addedit_sortorder.setText(Integer.toString(task.sortOrder))
+            } else {
+                //no task so we must be adding new task, and editing existing one
+                Log.d(TAG, "onViewCreated: no arguments adding new record")
+
+            }
+        }
+    }
+
+    private fun saveTask() {
+        //update db if at least 1 field is changed
+        var sortOrder = if (addedit_sortorder.text.isNotEmpty()){
+            Integer.parseInt(addedit_sortorder.text.toString())
+        } else {
+            0
+        }
+
+        val values = ContentValues()
+        val task = task
+
+        if (task != null) {
+            Log.d(TAG, "saveTask: updating existing task")
+            if(addedit_name.text.toString() != task.name) {
+                values.put(TasksContract.Columns.TASK_NAME, addedit_name.text.toString())
+            }
+            if(addedit_description.text.toString() != task.description) {
+                    values.put(TasksContract.Columns.TASK_DESCRIPTION, addedit_description.text.toString())
+            }
+            if(sortOrder != task.sortOrder) {
+                values.put(TasksContract.Columns.TASK_SORT_ORDER, sortOrder)
+            }
+            if (values.size() != 0) {
+                Log.d(TAG, "saveTask: updating task")
+                activity?.contentResolver?.update(TasksContract.buildUriFromId(task.id),
+                    values, null, null)
+
+            }
+        } else {
+            Log.d(TAG, "saveTask: adding new task")
+            if(addedit_name.text.isNotEmpty()) {
+                values.put(TasksContract.Columns.TASK_NAME, addedit_name.text.toString())
+                if (addedit_description.text.isNotEmpty()) {
+                    values.put(TasksContract.Columns.TASK_DESCRIPTION, addedit_description.text.toString())
+                }
+                values.put(TasksContract.Columns.TASK_SORT_ORDER, sortOrder)
+                activity?.contentResolver?.insert(TasksContract.CONTENT_URI, values)
+            }
+        }
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onActivityCreated starts")
+        Log.d(TAG, "onActivityCreated: starts")
         super.onActivityCreated(savedInstanceState)
+
+        //back button
+        if(listener is AppCompatActivity) {
+            var actionBar = (listener as AppCompatActivity?)?.supportActionBar
+            actionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
         addedit_save.setOnClickListener {
+            saveTask()
             listener?.onSaveClicked()
         }
     }
 
     override fun onAttach(context: Context) {
-        Log.d(TAG, "onAttach starts")
-
+        Log.d(TAG, "onAttach: starts")
         super.onAttach(context)
         if (context is OnSaveClicked) {
             listener = context
@@ -62,8 +131,7 @@ class AddEditFragment : Fragment() {
     }
 
     override fun onDetach() {
-        Log.d(TAG, "onDetach starts")
-
+        Log.d(TAG, "onDetach: starts")
         super.onDetach()
         listener = null
     }
@@ -88,7 +156,7 @@ class AddEditFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param task to be edited or null to add a new task
+         * @param task The task to be edited, or null to add a new task.
          * @return A new instance of fragment AddEditFragment.
          */
         @JvmStatic
@@ -99,23 +167,45 @@ class AddEditFragment : Fragment() {
                 }
             }
     }
-}
 
-//fun createFrag(task: Task) {
-//    val args = Bundle()
-//    args.putParcelable(ARG_TASK, task)
-//    val fragment = AddEditFragment()
-//    fragment.arguments = args
-//}
-//
-//fun createFrag2(task: Task) {
-//    val fragment = AddEditFragment().apply {
-//        arguments = Bundle().apply {
-//            putParcelable(ARG_TASK, task)
-//        }
-//    }
-//}
-//
-//fun simpler(task: Task) {
-//    val fragment = AddEditFragment.newInstance(task)
-//}
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewStateRestored: called")
+        super.onViewStateRestored(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView: called")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/zimbimdev/tasktimer/MainActivity.kt
+++ app/src/main/java/com/zimbimdev/tasktimer/MainActivity.kt
@@ -17,51 +17,57 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity(), AddEditFragment.OnSaveClicked {
 
-//whether the activity is in 2-pane mode
+    // Whether or the activity is in 2-pane mode
+    // i.e. running in landscape, or on a tablet.
     private var mTwoPane = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         mTwoPane = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+        Log.d(TAG, "onCreate: twoPane is $mTwoPane")
 
-        var fragment = supportFragmentManager.findFragmentById(R.id.task_details_container)
+        val fragment = supportFragmentManager.findFragmentById(R.id.task_details_container)
         if(fragment != null) {
-            //there was an existing fragment to edit task, make sure panes are set correctly
-           showEditPane()
+            // There was an existing fragment to edit a task, make sure the panes are set correctly
+            showEditPane()
         } else {
             task_details_container.visibility = if(mTwoPane) View.INVISIBLE else View.GONE
             mainFragment.view?.visibility = View.VISIBLE
         }
+        Log.d(TAG, "onCreate: finished")
     }
 
     private fun showEditPane() {
         task_details_container.visibility = View.VISIBLE
-        // hide the left pane if in single pane view
+        // hide the left hand pane, if in single pane view
         mainFragment.view?.visibility = if(mTwoPane) View.VISIBLE else View.GONE
+
     }
 
     private fun removeEditPane(fragment: Fragment? = null) {
-        Log.d(TAG, "removeEditPane call")
+        Log.d(TAG, "removeEditPane called")
         if(fragment != null) {
             supportFragmentManager.beginTransaction()
                 .remove(fragment)
                 .commit()
         }
 
-        //set the visibility for the right hand pane
+        // Set the visibility of the right hand pane
         task_details_container.visibility = if(mTwoPane) View.INVISIBLE else View.GONE
-            // and show left pane
+        // and show the left hand pane
         mainFragment.view?.visibility = View.VISIBLE
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
     }
 
     override fun onSaveClicked() {
-        Log.d(TAG, "onSaveClicked called")
-        var fragment = supportFragmentManager.findFragmentById(R.id.task_details_container)
+        Log.d(TAG, "onSaveClicked: called")
+        val fragment = supportFragmentManager.findFragmentById(R.id.task_details_container)
         removeEditPane(fragment)
     }
 
@@ -76,16 +82,22 @@ class MainActivity : AppCompatActivity(), AddEditFragment.OnSaveClicked {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
-            R.id.menumain_task -> taskEditRequest(null)
+            R.id.menumain_addTask -> taskEditRequest(null)
 //            R.id.menumain_settings -> true
+
+            android.R.id.home -> {
+                Log.d(TAG, "onOptionsItemSelected: home button pressed")
+                val fragment = supportFragmentManager.findFragmentById(R.id.task_details_container)
+                removeEditPane(fragment)
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
     private fun taskEditRequest(task: Task?) {
-        Log.d(TAG, "taskEditRequest starts")
+        Log.d(TAG, "taskEditRequest: starts")
 
-        //create new fragment to edit the task
+        // Create a new fragment to edit the task
         val newFragment = AddEditFragment.newInstance(task)
         supportFragmentManager.beginTransaction()
             .replace(R.id.task_details_container, newFragment)
@@ -93,6 +105,50 @@ class MainActivity : AppCompatActivity(), AddEditFragment.OnSaveClicked {
 
         showEditPane()
 
-        Log.d(TAG, "taskEditRequest ends")
+        Log.d(TAG, "Exiting taskEditRequest")
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onRestoreInstanceState: called")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onBackPressed() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.task_details_container)
+        if(fragment == null || mTwoPane) {
+            super.onBackPressed()
+        } else {
+            removeEditPane(fragment)
+        }
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
     }
 }
